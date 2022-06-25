9c51696f66874d56747e2c800f9258313040939c
NiTHiN6282 <nithindileep6282@gmail.com>
2022-06-25 12:59:39 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooddeliveryadmin/pages/foodlistpage.dart';
 import 'package:fooddeliveryadmin/screens/home.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: FoodListPage(),
     );
   }
 }
